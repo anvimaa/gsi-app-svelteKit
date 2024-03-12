a6e647d9e82ef01f96b0a053cc5480ca5d193cb7
anvimaa <anvimaa@gmail.com>
2024-03-12 14:56:50 +0100

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<div class="flex items-center justify-center">
-	<h1>Nada Para ver aqui por enquanto</h1>
+<div class="flex items-center justify-center h-screen">
+	<div class="text-center">
+		<p>Nada por enquanto</p>
+		<a href="/login" class="mt-4 btn btn-sm variant-outline-primary">Entrar</a>
+	</div>
 </div>
